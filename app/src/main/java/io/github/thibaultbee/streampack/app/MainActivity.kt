cf1bdafd39140bd22caca32f1f499631a601e992
alb-dam <alberto.damico01@gmail.com>
2026-03-21 16:17:43 +0100

    feat: add Context import to MainActivity.

--- app/src/main/java/io/github/thibaultbee/streampack/app/MainActivity.kt
+++ app/src/main/java/io/github/thibaultbee/streampack/app/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.content.Context
 import android.media.AudioManager
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CameraCharacteristics
